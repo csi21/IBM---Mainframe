72bfb47656059f6d4fa668605c2619ef7c5070b3
csi21 <csi13@imperial.ac.uk>
2018-11-14 22:29:11 +0000

    Update intro.cbl

--- Part One/intro.cbl
+++ Part One/intro.cbl
@@ -4,17 +4,17 @@
 
 *Log on to z/OS ISPF
 
- *- 3270 emulator software prerequisites
- *- Submit JCL to Allocate Part 1 Data Sets
- *- Verify Successful Allocation of Part 1 Data Sets
- *- Using ISPF to view Unix File Path and Files
- *- Create and Copy Unix File to a Data Set
- *- Get credit for completing Part 1
- *- How to terminate ISPF session and logoff
-*Part 1 introduced terminology and basics used in Part 2:
+ * - 3270 emulator software prerequisites
+ * - Submit JCL to Allocate Part 1 Data Sets
+ * - Verify Successful Allocation of Part 1 Data Sets
+ * - Using ISPF to view Unix File Path and Files
+ * - Create and Copy Unix File to a Data Set
+ * - Get credit for completing Part 1
+ * - How to terminate ISPF session and logoff
+* Part 1 introduced terminology and basics used in Part 2:
 
-*For more background and information about mainframes and to see examples of how important the mainframe is, check out the following web site:
+* For more background and information about mainframes and to see examples of how important the mainframe is, check out the following web site:
 
-*https://developer.ibm.com/academic
-*Thanks for participating, and good luck in Part 2!
+* https://developer.ibm.com/academic
+* Thanks for participating, and good luck in Part 2!
 
